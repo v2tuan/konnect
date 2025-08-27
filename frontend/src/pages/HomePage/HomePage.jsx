f268d7f805592afadb4f06cd30f413eb809265d3
DangDuyy <duyprovn987@gmail.com>
2025-08-28 06:50:14 +0700

    upgrate sidebar

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,16 @@
 import NavBar from '@/components/common/NavBar'
+import { AppSidebar } from '@/components/common/Sidebar/app-sidebar'
+import { SidebarProvider } from '@/components/ui/sidebar'
 
 function HomePage() {
   return (
-    <NavBar/>
+    <>
+      <SidebarProvider>
+
+        <AppSidebar/>
+        <NavBar/>
+      </SidebarProvider>
+    </>
   )
 }
 
