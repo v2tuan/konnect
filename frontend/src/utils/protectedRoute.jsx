6827ad5e4eb5412e3ce53363bbaf3dba049eb2da
DangDuyy <duyprovn987@gmail.com>
2025-08-28 07:02:37 +0700

    refactor protect route

--- frontend/src/utils/protectedRoute.jsx
+++ frontend/src/utils/protectedRoute.jsx
@@ -1,28 +1,19 @@
+import { selectCurrentUser } from "@/redux/user/userSlice"
+import { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
 import { Navigate, Outlet } from "react-router-dom"
-import { useState, useEffect } from "react"
-import axios from "axios"
-import { API_ROOT } from "@/utils/constant"
 
 export default function ProtectedRoute() {
   const [isAuthenticated, setIsAuthenticated] = useState(null) // null: loading, true: authenticated, false: not authenticated
 
-  useEffect(() => {
-    const checkAuth = async () => {
-      try {
-        const response = await axios.get(`${API_ROOT}/api/auth/check`, { withCredentials: true })
-        if (response.status === 200 && response.data.authenticated) {
-          setIsAuthenticated(true)
-        } else {
-          setIsAuthenticated(false)
-        }
-      // eslint-disable-next-line no-unused-vars
-      } catch (error) {
-        setIsAuthenticated(false)
-      }
-    }
+  const currentUser = useSelector(selectCurrentUser)
 
-    checkAuth()
-  }, [])
+  useEffect(() => {
+    if (currentUser)
+      setIsAuthenticated(true)
+    else
+      setIsAuthenticated(false)
+  }, [currentUser])
 
   if (isAuthenticated === null) {
     return <div>Loading...</div> // Hoặc một spinner/loading indicator
