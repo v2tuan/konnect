cfd93de9d077545a0e063e374e83f08faa24717f
DangDuyy <duyprovn987@gmail.com>
2025-10-01 22:20:50 +0700

    fix bug load list message

--- backend/src/models/conversation_members.js
+++ backend/src/models/conversation_members.js
@@ -1,32 +1,41 @@
 import mongoose from "mongoose";
 
 let conversationMemberSchema = new mongoose.Schema({
-    conversation: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Conversation',
-        required: true
-    },
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    nickname: {
-        type: String
-    },
-    role: {
-        type: String,
-        enum: ['member', 'admin', 'owner'],
-        default: 'member'
-    },
-    joinedAt: {
-        type: Date,
-        default: Date.now
-    },
-    lastReadMessageSeq: {
-        type: Number,
-        default: 0
-    }
+  conversation: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Conversation',
+    required: true
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['owner', 'admin', 'member'],
+    default: 'member'
+  },
+  lastReadMessageSeq: {
+    type: Number,
+    default: 0
+  },
+  deletedAt: {
+    type: Date,
+    default: null
+  },
+  deletedAtSeq: {
+    type: Number,
+    default: null
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 let ConversationMember = mongoose.model('ConversationMember', conversationMemberSchema);
